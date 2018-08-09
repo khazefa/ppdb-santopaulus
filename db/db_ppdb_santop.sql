6912163c5781ba1429e4154533740c94c7dbf8ad
khazefion <cybergitt@gmail.com>
2018-08-09 14:43:25 +0700

    CHange db

--- db/db_ppdb_santop.sql
+++ db/db_ppdb_santop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 10, 2018 at 04:45 PM
+-- Generation Time: Jul 17, 2018 at 01:51 PM
 -- Server version: 5.7.19
 -- PHP Version: 7.1.18
 
